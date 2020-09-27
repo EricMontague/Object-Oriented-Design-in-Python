dc86d33cef287a921ee1e72cbbceae9200cdcae8
Eric Montague <eric.g.montague@gmail.com>
2020-09-26 23:15:35 -0400

    Added methods to both classes for detecting if a method was read

--- online_chat/message.py
+++ online_chat/message.py
@@ -7,6 +7,7 @@ class Message:
         self._to_user = to_user
         self._text = text
         self._timestamp = datetime.now()
+        self._read = False
 
     @property
     def from_user(self):
@@ -28,12 +29,20 @@ class Message:
     def message_id(self):
         return self._message_id
 
+    def was_read(self):
+        return self._read
+    
+    def mark_as_read(self):
+        self._read = True
+
+
 
 class GroupMessage:
     def __init__(self, message_id, from_user, text):
         self._message_id = message_id
         self._from_user = from_user
         self._text = text
+        self._read_by = []
 
     @property
     def from_user(self):
@@ -46,3 +55,6 @@ class GroupMessage:
     @property
     def message_id(self):
         return self._message_id
+    
+    def mark_as_read_by(self, user):
+        self._read_by.append(user)
